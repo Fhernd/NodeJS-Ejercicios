1977a1d05854b79c234debe6dbf04d2d6a6a1af3
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-09 19:32:51 -0500

    Objeto con rutas de un servidor HTTP

--- cap001_intro/ej06_routing/solicitudes.js
+++ cap001_intro/ej06_routing/solicitudes.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
-function index(request, response) {
-    response.writeHead(200);
-    response.end('NodeJS');
+const routes = {
+    '/': function index(request, response) {
+        response.writeHead(200);
+        response.end('NodeJS');
+    },
+    '/saludo': function saludar(request, response) {
+        response.writeHead(200);
+        response.end('¡Hola!');
+    }
 }
 
 http.createServer(function (request, response) {
-    if (request.url === '/') {
-        return index(request, response);
+    if (request.url in routes) {
+        return routes[request.url](request, response);
     }
-
 
     response.writeHead(404);
     response.end(http.STATUS_CODES[404]);
